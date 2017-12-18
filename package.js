d58abbc18970d06af6aadfc30fe0ac7ba777a9e5
Firefaul <34631515+Firefaul@users.noreply.github.com>
2017-12-17 18:57:08 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "firebot",
+  "version": "1.0.0",
+  "description": "aaaa",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Jeta \"Firefaul\" Ryman",
+  "license": "ISC",
+  "dependencies": {
+    "discord": "^0.8.2"
+  }
+}
