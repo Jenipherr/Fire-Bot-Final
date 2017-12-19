3222295286ad88d268118e25273053173082ee7b
Firefaul <34631515+Firefaul@users.noreply.github.com>
2017-12-18 18:11:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ var Bot = new Discord.Client;
 var StartChannel = Bot.channels.get(StartupChannelId);
 */
 
+var Insult1 = ["hair", "face", "arm", "leg", "head"];
+var Insult2 = ["stupid", "ugly", "skinny", "thicc", "invisible", "short", "long"];
+var Insult3 = ["you can't ride a bike", "your mirror breaks when you look at it", "it's nonexistant", "you're stupid", "you look like Peter Pan"];
+
+
 var Owner = "229403861721415681";
+
+// Functions
+
+function RandomFromTable(Table) { 
+    var Random = Math.floor( ( Math.random() * (Table.length)) + 1);
+    return Table[Random];
+};
 
 // Initialize
 
@@ -17,6 +29,7 @@ Bot.on("ready", function() {
 	var StartupChannelId = "389963025722900484";
 	var StartChannel = Bot.channels.get(StartupChannelId);
 	StartChannel.send("FireBot initialized!");
+	Bot.user.setGame("!fire help");
 });
 
 Bot.on('message', message => {
@@ -29,13 +42,16 @@ Bot.on('message', message => {
 			"fields": [
 				{"name": "!fire ping", "value": "I'll reply with 'Pong' after I register what you said!", "inline": true},
 				{"name": "!fire help", "value": "I'll message you with what you're seeing right here!", "inline": true},
-				{"name": "!fire roastme", "value": "I'll attempt to roast you with some lit insults, fam!", "inline": true},
-				{"name": "!fire roast @user", "value": "I'll attempt to roast another user, similarly to !fire roastme!", "inline": true}
+				{"name": "!fire roastme", "value": "I'll attempt to roast you with some lit insults, fam!", "inline": true}
+				//{"name": "!fire roast @user", "value": "I'll attempt to roast another user, similarly to !fire roastme!", "inline": true}
 			],
 			"color": 15879226
 		}
     });
   };
+  if (message.content.toLowerCase()) == "!fire roastme") {
+	message:reply("your "+RandomFromTable(Insult1)+" is so "..RandomFromTable(Insult2)+", "+RandomFromTable(Insult3)+".")	  
+  }
 });
 
 Bot.login(process.env.BOT_TOKEN);
