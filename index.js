3e98548151a8241d7f76406bab6714e48632f6c2
Firefaul <34631515+Firefaul@users.noreply.github.com>
2018-01-04 13:51:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var Bot = new Discord.Client;
 var StartChannel = Bot.channels.get(StartupChannelId);
 */
 
-var Insult1 = ["hair", "face", "arm", "leg", "head", "mom"];
-var Insult2 = ["stupid", "ugly", "skinny", "thicc", "invisible", "short", "long"];
-var Insult3 = ["you can't ride a bike", "your mirror breaks when you look at it", "it's nonexistant", "you're stupid", "you look like Peter Pan"];
+var Insult1 = ["hair", "face", "arm", "leg", "head", "mom", "dick"];
+var Insult2 = ["stupid", "ugly", "skinny", "thicc", "invisible", "short", "long", "shit", "bitchy"];
+var Insult3 = ["you can't ride a bike", "your mirror breaks when you look at it", "it's nonexistant", "you're stupid", "you look like Peter Pan", "it's shit", "it's bitchy"];
 
 var Owner = "229403861721415681";
 
